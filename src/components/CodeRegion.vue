02c0458c862b10fdce495a4616ab889b4a6f88ee
徐霄雯 <sylviehsu@admindeMacBook-Pro.local>
2019-08-21 10:50:01 +0800

    first commit

---
+++ src/components/CodeRegion.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Samples</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CodeRegion',
+  data () {
+    return {
+      msg: 'Welcome to CodeRegion'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
